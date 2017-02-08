af9ad6f783b0dfdd373d00714a39c3f1963e0cb5
Prabh160 <prabhsingh160@gmail.com>
2017-02-07 19:29:30 -0800

    pages built & bootstrap applied

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Prabh Portfolio' });
 });
+
+/*GET Projects*/
+router.get('/projects', function(req,res,next){
+    res.render('projects');
+});
+
+/*GET Services*/
+router.get('/services', function(req,res,next){
+    res.render('services');
+});
+
+/*GET About*/
+router.get('/about', function(req,res,next){
+    res.render('about');
+});
+
+/*GET Contact*/
+router.get('/contact', function(req,res,next){
+    res.render('contact');
+});
+
 
 module.exports = router;
