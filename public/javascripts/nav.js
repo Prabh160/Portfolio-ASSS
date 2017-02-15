6e4b3a4e91095f16e3b8cf6f63d4b3f4d511b4eb
Prabh160 <prabhsingh160@gmail.com>
2017-02-14 20:41:34 -0800

    Re-Design & Images

---
+++ public/javascripts/nav.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Sharpshooter on 2017-02-14.
+ */
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
